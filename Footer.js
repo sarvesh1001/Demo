e6c63ab0451d3662557e9b039ceca7805ee66e4c
sarvesh1001 <savichhabra05@gmail.com>
2024-03-20 09:53:40 +0530

    added

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer=()=>{
+    return(
+        <div>
+            <h3 className='footer'>E-comm dashboard</h3>
+        </div>
+    );
+}
+export default Footer;
